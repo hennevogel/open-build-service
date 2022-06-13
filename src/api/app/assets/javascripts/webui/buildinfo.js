68fc617d605946952199f6266aa8770a5e728ea7
dleidi <dleidi@suse.com>
2022-06-13 15:38:52 +0200

    Lint fixes

--- src/api/app/assets/javascripts/webui/buildinfo.js
+++ src/api/app/assets/javascripts/webui/buildinfo.js
@@ -1,30 +1,27 @@
-class Package {
-  constructor(name, sources, requiredBy) {
-    this.name = name;
-    this.sources = sources;
-    this.requiredBy = requiredBy;
-  }
+function Package(name, sources, requiredBy) {
+  return {
+    name: name,
+    sources: sources,
+    requiredBy: requiredBy
+  };
 }
 
 // utility classes to generate HTML
 function th(content) {
-  return `<th>${content}</th>`;
+  return '<th>' + content + '</th>';
 }
 function tableHead() {
-  let content = '';
-  for (let i = 0; i < arguments.length; i++) {
+  var content = '';
+  for (var i = 0; i < arguments.length; i++) {
     content += th(arguments[i]);
   }
-  return `
-    <thead>
-      ${row(content)}
-    </thead>`;
+  return '<thead>' + row(content) + '</thead>';
 }
 function col(htmlContent, styleClass) {
-  return `<td class="${styleClass}">${htmlContent}</td>`;
+  return '<td class="' + styleClass + '">' + htmlContent + '</td>';
 }
 function row(htmlContent) {
-  return `<tr>${htmlContent}</tr>`;
+  return '<tr>' + htmlContent + '</tr>';
 }
 
 // main
@@ -32,15 +29,15 @@ function normalizeData() {
   var rawData = $('#build-info-raw-data').text();
 
   // relevant entities to compute on
-  const _PACKAGES = [];
-  const _DISTINCT_SOURCES = new Set();
-  let _DIRECT_DEPENDENCIES;
+  var _PACKAGES = [];
+  var _DISTINCT_SOURCES = new Set();
+  var _DIRECT_DEPENDENCIES;
 
   // encoding and shaping newline properly
   var normalizedData = rawData.replace(/\\n/g, '\n');
   normalizedData = normalizedData.replace(/\",/g, '",\n');
 
-  const errorMessage = normalizedData.match(/\"error\"\=\>\"(.*)\"/)[1];
+  var errorMessage = normalizedData.match(/\"error\"\=\>\"(.*)\"/)[1];
   $('#error-message').text(errorMessage); // summary
 
   _DIRECT_DEPENDENCIES = normalizedData.match(/expand args: (.*)/)[1].split(' ');
@@ -48,38 +45,40 @@ function normalizeData() {
 
   // populate the dependencies table
   $('#dependency-relationship').html(() => {
-    let chainedDependenciesHtml = `<table>${tableHead('who', 'requires', 'what', 'from')}`;
+    var chainedDependenciesHtml = '<table>' + tableHead('who', 'requires', 'what', 'from');
     normalizedData.match(/added (.*) because of (.*)/g)
       .forEach(element => {
-        const matchingGroupsForAddedPackages = element.match(/added (.*) because of (.*)/);
+        var matchingGroupsForAddedPackages = element.match(/added (.*) because of (.*)/);
         if (matchingGroupsForAddedPackages.length > 0) {
-          const extendedPackageName = matchingGroupsForAddedPackages[1];
-          const pkgName = extendedPackageName.split('@')[0];
-          const pkgSource = extendedPackageName.split('@')[1];
-          const requiredBy = matchingGroupsForAddedPackages[2];//.replace('(direct):', '');
+          var extendedPackageName = matchingGroupsForAddedPackages[1];
+          var pkgName = extendedPackageName.split('@')[0];
+          var pkgSource = extendedPackageName.split('@')[1];
+          var requiredBy = matchingGroupsForAddedPackages[2];//.replace('(direct):', '');
 
           // deduplicate packages
           if (_PACKAGES.filter(p => p.name === pkgName).length > 0) {
-            const package = _PACKAGES.find(p => p.name === pkgName);
+            var package = _PACKAGES.find(p => p.name === pkgName);
             package.requiredBy.push(requiredBy);
             package.sources.push(pkgSource);
           }
           else {
-            _PACKAGES.push(new Package(pkgName, [pkgSource], [requiredBy]));
+            _PACKAGES.push(Package(pkgName, [pkgSource], [requiredBy]));
           }
 
           _DISTINCT_SOURCES.add(pkgSource); // keep track of distinct sources
 
           chainedDependenciesHtml +=
             row(
-              (_DIRECT_DEPENDENCIES.includes(requiredBy) ? col(`<strong>${requiredBy}</strong>`) : col(requiredBy)) +
+              (_DIRECT_DEPENDENCIES.includes(requiredBy) ?
+                col('<strong>' + requiredBy + '</strong>')
+                : col(requiredBy)) +
               col('-->', 'text-center') +
               col(pkgName) +
-              col(`<span class="text-info">${pkgSource}</span>`)
+              col('<span class="text-info">' + pkgSource + '</span>')
             );
         }
     });
-    if (_PACKAGES.length == 0) {
+    if (_PACKAGES.length === 0) {
       chainedDependenciesHtml += row(col('no packages found'));
     }
     chainedDependenciesHtml += '</table>';
@@ -89,15 +88,16 @@ function normalizeData() {
 
   // populate project sources and packages
   $('#sources').html(() => {
-    let sourcesHtml = '';
+    var sourcesHtml = '';
     _DISTINCT_SOURCES.forEach(source => {
-      const packagesSubset = _PACKAGES.filter(p => p.sources.includes(source));
-      sourcesHtml += `<div class="text-info source-name collapsed">${source} (${packagesSubset.length})</div>`
-      sourcesHtml += `<ul class="source-package-list collapsed">`
+      var packagesSubset = _PACKAGES.filter(p => p.sources.includes(source));
+      sourcesHtml += '<div class="text-info source-name collapsed">' +
+          source + ' (' + packagesSubset.length + ')</div>';
+      sourcesHtml += '<ul class="source-package-list collapsed">';
       packagesSubset.forEach(package => {
-          sourcesHtml += `<li>${package.name}</li>`
+          sourcesHtml += '<li>' + package.name + '</li>';
         });
-        sourcesHtml += `</ul>`;
+        sourcesHtml += '</ul>';
     });
     return sourcesHtml;
   });
@@ -109,8 +109,11 @@ function normalizeData() {
   });
   $('#show-raw-data').on('click', function() {
     $('#build-info-raw-data').toggleClass('collapsed');
-    $('#build-info-raw-data').hasClass('collapsed') ?
-      $(this).text('show raw data') :
+    if ($('#build-info-raw-data').hasClass('collapsed')) {
+      $(this).text('show raw data');
+    }
+    else {
       $(this).text('hide raw data');
+    }
   });
 }
